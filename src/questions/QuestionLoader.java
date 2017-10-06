49d51bc8e043a24f705d09069cedda05ee51d409
nca1424 <nca1424@gmail.com>
2017-10-05 23:26:12 -0500

    Updated QuestionLoader to include iterator

--- src/questions/QuestionLoader.java
+++ src/questions/QuestionLoader.java
@@ -3,6 +3,7 @@ package questions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -27,7 +28,15 @@ public class QuestionLoader {
 	
 	public static void initializeQuestions () throws FileNotFoundException, IOException, ParseException{
 		JSONParser parser = new JSONParser();
-		JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("test.json"));
+		JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("./src/test.json"));
+		
+		JSONArray questions = (JSONArray) jsonObject.get("questions");
+		
+		Iterator<JSONObject> iterator = questions.iterator();
+        while (iterator.hasNext()) {
+        	JSONObject someQuestion = iterator.next();
+        		System.out.println(someQuestion.toString());
+        	}
 		
 
 	}
