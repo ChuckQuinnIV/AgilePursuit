069ab9154359bc2cf2c429f64213e47a0917e07e
Nicholas <nsstanley51@gmail.com>
2017-10-18 17:19:46 -0500

    added getPlayer methond to PlayerMgr

--- src/player/PlayerMgr.java
+++ src/player/PlayerMgr.java
@@ -2,27 +2,25 @@ package player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import questions.Question;
 
 public class PlayerMgr {
 	
 	private HashMap<Integer,Player> allPlayers = new HashMap<Integer,Player>();
 	
-	
 	@Override
 	public String toString() {
 		return allPlayers.toString();
-	
 	}
-
+	
 	public void addPlayer(Player n){
 		int id = n.getId();
 		Player value = allPlayers.get(id);
 		if (value != null) { throw new IllegalArgumentException(id + " is already in the all Players hashmap");}
 		allPlayers.put(n.getId(), n);
 	}
-
 	
-
+	public Player getPlayer(int id){
+		return allPlayers.get(id);
+	}
 }
