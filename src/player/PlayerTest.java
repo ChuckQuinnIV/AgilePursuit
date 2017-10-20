8ba2daa745977bde7a4b13924204b909799d50b2
Nicholas <nsstanley51@gmail.com>
2017-10-20 09:42:47 -0500

    completly change the PlayerTest object to test the new changes in PlayerMgr.java

--- src/player/PlayerTest.java
+++ src/player/PlayerTest.java
@@ -11,68 +11,28 @@ public class PlayerTest {
 		
 		PlayerMgr PlayerManager = new PlayerMgr();
 		
-		HashMap<Integer, Player> localPlayers; 
+		System.out.println(PlayerManager.containsPlayer(1));
+		System.out.println(PlayerManager.containsPlayer(1));
+
 		
+		PlayerManager.clear();
+		System.out.println(PlayerManager.toString());
+				
 		Player p1 = new Player(1,"nstanley", new ArrayList<String>(), 1,5);
 		PlayerManager.addPlayer(p1);
-
 		Player p2 = new Player(2,"aeinstein", new ArrayList<String>(), 5,5);
 		PlayerManager.addPlayer(p2);
-		
 		Player p3 = new Player(3,"cdarwin", new ArrayList<String>(), 3,5);
 		PlayerManager.addPlayer(p3);
-		
-		localPlayers = PlayerManager.getHash();
 		PlayerManager.saveFile();
-		
-		System.out.println(PlayerManager.toString());
-		
-		localPlayers.clear();
-		
-		System.out.println(PlayerManager.toString());
-		
-		try {
-			PlayerManager.setHash(PlayerManager.readFile());
-			localPlayers = PlayerManager.getHash();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		System.out.println(PlayerManager.toString());
-		
-		Player p4 = new Player(5, "testName", new ArrayList<String>(), 4,6);
+		Player p4 = new Player(4, "testName", new ArrayList<String>(), 4,6);
 		PlayerManager.addPlayer(p4);
-		
 		PlayerManager.saveFile();
-		
-		try {
-			PlayerManager.setHash(PlayerManager.readFile());
-			localPlayers = PlayerManager.getHash();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		System.out.println(PlayerManager.toString());
-		System.out.println(localPlayers.keySet());
-		System.out.println(localPlayers.values());
+
 		
-		localPlayers = PlayerManager.getHash();
-		System.out.println(localPlayers);
-		System.out.println(localPlayers.size());
 		
-		/*System.out.print("Enter Username: ");
-		Scanner sc = new Scanner(System.in);
-		String username = sc.nextLine();
-		*/
-		
-		Player localPlayer = localPlayers.get(1);
-		System.out.println(localPlayer.toString());
-		localPlayer.incrementGamesWon();
-		localPlayer.incrementGamesTotal();
-		localPlayer.addGroup("TestTeam1");
-		System.out.println(localPlayer.toString());
+
 		
 
 	}
